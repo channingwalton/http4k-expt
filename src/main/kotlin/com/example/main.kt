6c49331f2e24d2b07b866fd3e8404506703cfa9d
Channing Walton <channingwalton@mac.com>
2025-01-10 10:01:43 +0000

    Refactoring

--- src/main/kotlin/com/example/main.kt
+++ src/main/kotlin/com/example/main.kt
@@ -1,6 +1,5 @@
 package com.example
 
-import org.http4k.core.HttpHandler
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters.PrintRequest
 import org.http4k.server.SunHttp
@@ -13,9 +12,7 @@ fun main() {
   Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
   transaction { SchemaUtils.create(Customers) }
   val handlers = CustomerHandler()
-  val printingApp: HttpHandler = PrintRequest().then(handlers.app)
-
-  val server = printingApp.asServer(SunHttp(9000)).start()
-
+  val app = PrintRequest().then(handlers.app)
+  val server = app.asServer(SunHttp(9000)).start()
   println("Server started on " + server.port())
 }
